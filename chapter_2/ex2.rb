87824ba1f029c9ed1682f7beb131650f1dee8274
Lucas Henderson <lucasjhenderson@gmail.com>
2015-10-01 17:49:25 -0600

    Added ex2.rb, tested code in iTerm output, ran gaurd
    [#104389488]

---
+++ chapter_2/ex2.rb
@@ -0,0 +1,13 @@
+# A Comment
+# Octothorpe hides the text after it in a single line
+
+puts 'I could have code like this.' # And the comment after is ignored
+
+# You can also use a comment to 'disable' or comment out a line of code:
+# puts 'this won't run'
+
+puts 'This will run.'
+puts ''
+puts 'Can you find a key combo in RubyMine to comment out a line of code?'
+puts ''
+puts 'Indeed. Key combo "Cmd+/" does the trick'

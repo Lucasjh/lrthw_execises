40e62971766ab11895614773cfc920d7760be3b3
Lucas Henderson <lucasjhenderson@gmail.com>
2015-10-02 17:28:49 -0600

    Ran guard and fixed offenses in both .rb's
    [#104389490]

--- chapter_3/ex3.rb
+++ chapter_3/ex3.rb
@@ -1,16 +1,19 @@
 puts 'I will now count my chickens:'
 
-# divides 30 by 6 and adds 25. #{x-y+z} must 'escape' the math phrase or function from the string to allow a number to be calculated.
+# divides 30 by 6 and adds 25.
+# #{x-y+z} must 'escape' the math phrase or function from the
+# string to allow a number to be calculated.
 puts "Hens #{25 + 30 / 6}"
 puts "Roosters #{100 - 25 * 3 % 4}"
 
-puts "Now I will count the eggs:"
+puts 'Now I will count the eggs:'
 # Here is just the number bieng recognized by ruby as integers, or fixnums
 puts 3 + 2 + 1 + - 5 + 4 % 2 - 1 / 4 + 6
 
-puts "Is it true that 3 + 2 < 5 - 7?"
+puts 'Is it true that 3 + 2 < 5 - 7?'
 
-# From what i know using <,>, <=, and >= returns a logic value of true or false when measuring integers on either side.
+# From what i know using <,>, <=, and >= returns a logic
+# value of true or false when measuring integers on either side.
 puts 3 + 2 < 5 - 7
 # Same as above here
 puts "What is 3 + 2? #{3 + 2}"
